6cac43db147be65d0371de12c561100484b934e8
Stian Morsund <stian@stianmorsund.no>
2018-12-12 20:50:11 +0100

    add clock to abstract class

--- src/models/three.abstract.ts
+++ src/models/three.abstract.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
 export abstract class ThreeModel {
-  abstract update(): void; // all models must inherit update for rendering
+  abstract update(clock?: THREE.Clock): void; // all models must inherit update for rendering
   abstract mesh: THREE.Mesh;
 }
