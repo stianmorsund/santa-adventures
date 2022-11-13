dc6665c1ed3abc5530d671159ba9c3457094a071
stianmorsund <stian.morsund@webstep.no>
2022-11-13 23:05:24 +0100

    refactor state from track to index

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,10 @@ import { Gift } from './meshes/gift';
 import { Forest } from './meshes/forest';
 import { Controls } from './controls/controls';
 import { LoadingManager } from './controls/loading-manager';
-import { Enemy } from './meshes/enemy';
+import { Hinder } from './meshes/hinder';
+import { LightHinder } from './meshes/light-hinder';
+import { getRandomInteger } from './utils/utils';
+import { NUMBER_OF_GIFTS } from './meshes/constants';
 
 // create the scene
 const scene: Scene = Scene.getInstance();
@@ -58,30 +61,57 @@ scene.addModel(forest);
 const snow = new Snow();
 scene.addModel(snow);
 
+let gifts: Gift[] = Array.from({ length: NUMBER_OF_GIFTS }, () => new Gift());
+let hinders: Hinder[] = [];
+let lightHinders: LightHinder[] = [];
+
+for (let i = 1; i < 10; i++) {
+  const position = {
+    x: 0,
+    y: i * 30,
+    z: 1,
+  };
+  lightHinders = [...(lightHinders || []), new LightHinder({ position })];
+}
+
+for (let i = 1; i < 10; i++) {
+  const x = getRandomInteger(-1, 1);
+  const y = i * 20;
+  const z = 0;
+  hinders = [...(hinders || []), new Hinder({ position: { x, y, z } })];
+}
+
+track.addModel(...hinders.map((h) => h.mesh));
+track.addModel(...gifts.map((g) => g.mesh));
+track.addModel(...lightHinders.map((l) => l.mesh));
+
 let previouslyCollected: Gift;
 
 /**
  * @returns the gift our hero collided with
  */
 function getCollectedGift(): Gift {
-  return track.gifts.find(
-    (g) => Math.floor(g.mesh.position.y) === 0 && g.mesh.position.x === hero.currentPosition && !hero.isJumbing
+  return gifts.find(
+    (g) => Math.floor(g.mesh.position.y * 2) === 0 && g.mesh.position.x === hero.currentPosition && !hero.isJumbing
   );
 }
 
-function isEnemyCollision(): boolean {
-  return track.enemies.some(
-    (e) => Math.floor(e.mesh.position.y) === 0 && e.mesh.position.x === hero.currentPosition && !hero.isJumbing
-  );
+function isHinderCollision(): boolean {
+  // return hinders.some(
+  //   (hinder) =>
+  //     // Math.floor(hinder.mesh.position.y) === 0 && hinder.mesh.position.x === hero.currentPosition && !hero.isJumbing
+  //     Math.floor(hinder.mesh.position.y * 2) === 0 && !hero.isJumbing
+  // );
+  return false;
 }
 
 // Orbit
 
-// let orbitControl = new OrbitControls(camera, renderer.domElement); //helper to rotate around in scene
-// orbitControl.addEventListener('change', render);
-// orbitControl.enableDamping = true;
-// orbitControl.dampingFactor = 0.8;
-// orbitControl.enableZoom = true;
+let orbitControl = new OrbitControls(camera, renderer.domElement); //helper to rotate around in scene
+orbitControl.addEventListener('change', render);
+orbitControl.enableDamping = true;
+orbitControl.dampingFactor = 0.8;
+orbitControl.enableZoom = true;
 
 window.addEventListener('resize', onWindowResize, false);
 
@@ -98,7 +128,7 @@ function animate(): void {
 
 function render(): void {
   if (!controls.isPlaying || !controls.isAlive) return;
-  controls.isAlive = !isEnemyCollision();
+  controls.isAlive = !isHinderCollision();
   if (!controls.isAlive) {
     controls.displayGameover();
   }
@@ -108,6 +138,10 @@ function render(): void {
       m.update(clock);
     });
   }
+
+  hinders.forEach((g) => g.update());
+  gifts.forEach((g) => g.update());
+  lightHinders.forEach((l) => l.update());
 
   const collected: Gift = getCollectedGift();
   if (collected) {
--- src/meshes/track.ts
+++ src/meshes/track.ts
@@ -1,49 +1,36 @@
 import * as THREE from 'three';
+import { FOREST_SPEED, TRACK_LENGTH } from './constants';
 import { MeshBase } from './meshbase.abstract';
-import { Scene } from '../scene';
-import { Gift } from './gift';
-import { TRACK_LENGTH } from './constants';
-import { Enemy } from './enemy';
-import { getRandomInteger } from '../utils/utils';
-import { EnemyBuilder } from './enemy-builder';
 
 export class Track extends MeshBase {
   geometry: THREE.PlaneGeometry;
   mesh: THREE.Mesh;
-  scene: Scene = Scene.getInstance();
-  gifts: Gift[] = [];
-  enemies: Enemy[] = [];
-  readonly NUMBER_OF_GIFTS = 5;
-  readonly NUMBER_OF_ENEMIES = 3;
 
   constructor() {
     super();
     this.geometry = new THREE.PlaneGeometry(10, TRACK_LENGTH, 32);
 
-    const texture = new THREE.TextureLoader().load(require('../assets/textures/snow.jpg'));
+    const texture = new THREE.TextureLoader().load(require('../assets/textures/snow_down.jpg'));
     texture.anisotropy = 4;
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.format = THREE.RGBFormat;
-    const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+    const material = new THREE.MeshLambertMaterial({ color: 0xffffff, map: texture });
     this.mesh = new THREE.Mesh(this.geometry, material);
     this.mesh.castShadow = true;
     this.mesh.receiveShadow = true;
     this.mesh.position.y = 0;
     this.mesh.position.z = 5;
     this.mesh.rotation.x = 4.75;
-
-    this.gifts = Array.from({ length: this.NUMBER_OF_GIFTS }, () => new Gift());
-    this.mesh.add(...this.gifts.map((g) => g.mesh));
-
-    const eb = new EnemyBuilder(this.NUMBER_OF_ENEMIES).build().then((enemies) => {
-      this.enemies = enemies;
-      this.mesh.add(...this.enemies.map((e) => e.mesh));
-    });
   }
 
   update(clock: THREE.Clock) {
-    this.gifts.forEach((g) => g.update());
-    this.enemies.forEach((e) => e.update());
+    
+    
+  }
+  addModel(...models: THREE.Mesh[]) {
+    models.forEach((m) => {
+      this.mesh.add(m);
+    });
   }
 }
