bf07c75b3c81a938f56676c6e2f112be0587b474
stianmorsund <stian.morsund@webstep.no>
2022-11-13 23:05:57 +0100

    refactor const

--- src/meshes/constants.ts
+++ src/meshes/constants.ts
@@ -3,3 +3,4 @@ export const TRACK_LENGTH = 200;
 export const GIFT_HEIGHT_FROM_FLOOR = 0.5;
 export const TRACK_SPEED = 0.2;
 export const FOREST_SPEED = TRACK_SPEED / 1.2; // forest speed, approx 20% slower than track speed
+export const NUMBER_OF_GIFTS = 20;
