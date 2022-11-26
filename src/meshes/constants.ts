e0ce0373e020ff8f99ad163cc105cc9f3020efc2
stianmorsund <stian.morsund@webstep.no>
2022-11-26 20:46:17 +0100

    rm unused consts

--- src/meshes/constants.ts
+++ src/meshes/constants.ts
@@ -1,8 +1,4 @@
-// Todo: Encapsulate some to correct files
-
-export const GROUND_LEVEL = 0
 export const TRACK_LENGTH = 200
 export const TRACKBASE_Z = 0.5
 export const TRACK_SPEED = 0.2
 export const FOREST_SPEED = TRACK_SPEED / 1.2 // forest speed, approx 20% slower than track speed
-export const NUMBER_OF_GIFTS = 20
