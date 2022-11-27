fb7aa0ef356bd8b2b271435d0c504a2c0f3062be
stianmorsund <stian.morsund@webstep.no>
2022-11-27 22:26:31 +0100

    rm util not needed

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,14 +5,3 @@ export function randomFiveChars(): string {
 export function getRandomInteger(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
-
-export function addCloseOverlayListener(overlay: HTMLElement, callback: Function) {
-  if (!overlay) {
-    return
-  }
-  overlay.addEventListener('click', function (ev) {
-    if (ev.composedPath()[0] === this) {
-      callback()
-    }
-  })
-}
