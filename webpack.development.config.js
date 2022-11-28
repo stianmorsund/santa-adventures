800c8397df96692418a86298ef8359f3ee5aba77
stianmorsund <stian.morsund@webstep.no>
2022-11-28 08:44:37 +0100

    cleanup webpack configs

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,35 +1,6 @@
-// const merge = require('webpack-merge');
-// const common = require('./webpack.config.js');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-// const BrowserSyncPluginConfig = new BrowserSyncPlugin(
-//   {
-//     host: '127.0.0.1',
-//     port: 3000,
-//     proxy: 'http://127.0.0.1:8080/',
-//   },
-//   (config = {
-//     reload: false,
-//   })
-// );
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
 
-// const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-// const ProgressBarPluginConfig = new ProgressBarPlugin();
-
-// module.exports = merge(common, {
-//   mode: 'development',
-//   target: 'web',
-//   devtool: 'source-map',
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, 'dist'),
-//     },
-//     compress: true,
-//     port: 9000,
-//   },
-//   plugins: [
-//     BrowserSyncPluginConfig,
-//     ProgressBarPluginConfig,
-//     new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
-//   ],
-// });
+module.exports = merge(common, {
+  mode: 'development',
+})
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,10 +1,6 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
-  output: {
-    publicPath: '/dev/santa-adventures/',
-  },
 })
