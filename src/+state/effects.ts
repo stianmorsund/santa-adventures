8cda19f68755d82bccad710d4875cc38532d0458
stianmorsund <stian.morsund@webstep.no>
2022-11-30 19:58:51 +0100

    refactor store and effects

---
+++ src/+state/effects.ts
@@ -0,0 +1,29 @@
+import { createListenerMiddleware } from '@reduxjs/toolkit'
+import { Camera } from '../camera/camera'
+import { reachedFinishline } from './santa-slice'
+import { pressedPlay, pressedRestartGame } from './ui-slice'
+
+export const listenerMiddleware = createListenerMiddleware()
+
+listenerMiddleware.startListening({
+  actionCreator: pressedPlay,
+  effect: async (_action) => {
+    // Show ccanvas when starting game
+    document.getElementsByTagName('canvas')[0].style.display = 'block'
+    document.body.classList.add('game-started')
+  },
+})
+
+listenerMiddleware.startListening({
+  actionCreator: pressedRestartGame,
+  effect: async (_action) => {
+    window.location.reload()
+  },
+})
+
+listenerMiddleware.startListening({
+  actionCreator: reachedFinishline,
+  effect: async (_action) => {
+    Camera.getInstance().setRotation({ x: -0.15, y: Math.PI, z: 0 }).setPosition({ x: 0, y: 1.2, z: -0.8 })
+  },
+})
--- src/+state/store.ts
+++ src/+state/store.ts
@@ -1,33 +1,8 @@
-import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit'
-import { Camera } from '../camera/camera'
+import { configureStore } from '@reduxjs/toolkit'
 import assetsSlice from './assets-slice'
-import santaReducer, { reachedFinishline } from './santa-slice'
-import uiSlice, { pressedPlay, pressedRestartGame } from './ui-slice'
-
-export const listenerMiddleware = createListenerMiddleware()
-
-listenerMiddleware.startListening({
-  actionCreator: pressedPlay,
-  effect: async (_action) => {
-    // Show ccanvas when starting game
-    document.getElementsByTagName('canvas')[0].style.display = 'block'
-    document.body.classList.add('game-started')
-  },
-})
-
-listenerMiddleware.startListening({
-  actionCreator: pressedRestartGame,
-  effect: async (_action) => {
-    window.location.reload()
-  },
-})
-
-listenerMiddleware.startListening({
-  actionCreator: reachedFinishline,
-  effect: async (_action) => {
-    Camera.getInstance().setRotation({ x: -0.15, y: Math.PI, z: 0 }).setPosition({ x: 0, y: 1.2, z: -0.8 })
-  },
-})
+import { listenerMiddleware } from './effects'
+import santaReducer from './santa-slice'
+import uiSlice from './ui-slice'
 
 export const store = configureStore({
   reducer: { ui: uiSlice, assets: assetsSlice, santa: santaReducer },
