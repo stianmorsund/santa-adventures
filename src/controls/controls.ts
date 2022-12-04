fd9092fbf00b75fdefab66943b99a06bf9c2581f
stianmorsund <stian.morsund@webstep.no>
2022-12-04 12:11:13 +0100

    controls as function

--- src/controls/controls.ts
+++ src/controls/controls.ts
@@ -2,48 +2,45 @@ import * as santa from '../+state/santa-slice'
 import { store } from '../+state/store'
 import { pressedCredits, pressedEscape } from '../+state/ui-slice'
 
-export class Controls {
-  constructor() {
-    document.onkeydown = (e) => this.handleKeyDown(e)
+export function addControls() {
+  document.onkeydown = (e) => handleKeyDown(e)
+}
+
+function handleKeyDown(event: KeyboardEvent) {
+  const { isJumping, isCrawling, santaPosition, isCurrentLevelFinished } = store.getState().santa
+  const { isCreditsOpened, hasGameStarted } = store.getState().ui
+  const { key } = event
+
+  if (isCurrentLevelFinished) return
+
+  if (key === 'Escape') {
+    if (isCreditsOpened) {
+      store.dispatch(pressedCredits())
+      return
+    }
+    store.dispatch(pressedEscape())
   }
 
-  handleKeyDown(event: KeyboardEvent) {
-    const { isJumping, isCrawling, santaPosition, isCurrentLevelFinished } = store.getState().santa
-    const { isCreditsOpened, hasGameStarted } = store.getState().ui
-    const { key } = event
-
-    // Controsl are disabled when level is finished
-    if (isCurrentLevelFinished) return
-
-    if (key === 'Escape') {
-      if (isCreditsOpened) {
-        store.dispatch(pressedCredits())
-        return
-      }
-      store.dispatch(pressedEscape())
-    }
-
-    if (!hasGameStarted || isJumping) return
-    switch (key) {
-      case 'a':
-      case 'ArrowLeft':
-        if (santaPosition === -1) return
-        store.dispatch(santa.movedLeft())
-        break
-      case 'd':
-      case 'ArrowRight':
-        if (santaPosition === 1) return
-        store.dispatch(santa.movedRight())
-        break
-      case ' ':
-      case 'ArrowUp':
-        store.dispatch(santa.jumped())
-        break
-      case 's':
-      case 'ArrowDown':
-        if (isCrawling) return
-        store.dispatch(santa.crawled())
-        break
-    }
+  if (!hasGameStarted || isJumping) return
+  switch (key) {
+    case 'a':
+    case 'ArrowLeft':
+      if (santaPosition === -1) return
+      store.dispatch(santa.movedLeft())
+      break
+    case 'd':
+    case 'ArrowRight':
+      if (santaPosition === 1) return
+      store.dispatch(santa.movedRight())
+      break
+    case ' ':
+    case 'ArrowUp':
+      store.dispatch(santa.jumped())
+      break
+    case 's':
+    case 'ArrowDown':
+      if (isCrawling) return
+      store.dispatch(santa.crawled())
+      break
   }
 }
