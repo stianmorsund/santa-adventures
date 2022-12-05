8e052a4e03bd35814b762e69f0713c4862a719c0
stianmorsund <stian.morsund@webstep.no>
2022-12-05 09:25:49 +0100

    give canvas an id

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,7 +6,7 @@ export function createRenderer(): THREE.WebGLRenderer {
   renderer.setSize(window.innerWidth, window.innerHeight)
   renderer.shadowMap.enabled = true
   renderer.shadowMap.type = THREE.PCFSoftShadowMap
-
+  renderer.domElement.setAttribute('id', 'game-canvas')
   document.body.appendChild(renderer.domElement)
   return renderer
 }
