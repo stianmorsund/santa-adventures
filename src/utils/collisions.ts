4ca1602077fe3aa764baeba4291887534efb6986
stianmorsund <stian.morsund@webstep.no>
2022-12-10 12:15:09 +0100

    refactor collisions

--- src/utils/collisions.ts
+++ src/utils/collisions.ts
@@ -4,26 +4,41 @@ import { Pole } from '../meshes/pole'
 import { Wall } from '../meshes/wall'
 import { SantaXPosition } from '../models/models'
 
+const SANTA_COLLISION_THRESHOLD = 2 // Can be increased so that collisins occurs further away from santa
+const SANTA_Y_POSITION = 0 // Never changes
+
 export function getCollectedGift({
   gifts,
-  santaPosition,
+  santaXPosition,
   isJumping,
 }: {
   gifts: Gift[]
-  santaPosition: SantaXPosition
+  santaXPosition: SantaXPosition
   isJumping: boolean
 }): Gift | undefined {
-  return gifts.find((g) => Math.floor(g.mesh.position.y * 2) === 0 && g.mesh.position.x === santaPosition && !isJumping)
+  return gifts.find(
+    (g) =>
+      Math.floor(g.mesh.position.y * SANTA_COLLISION_THRESHOLD) === SANTA_Y_POSITION &&
+      g.mesh.position.x === santaXPosition &&
+      !isJumping
+  )
 }
 
 export function isWallCollision({ walls, isJumping }: { walls: Wall[]; isJumping: boolean }): boolean {
-  return walls.some((wall) => Math.floor(wall.mesh.position.y * 2) === 0 && !isJumping)
+  return walls.some(
+    (wall) => Math.floor(wall.mesh.position.y * SANTA_COLLISION_THRESHOLD) === SANTA_Y_POSITION && !isJumping
+  )
 }
 
 export function isPoleCollision({ poles, isCrawling }: { poles: Pole[]; isCrawling: boolean }): boolean {
-  return poles.some((pole) => Math.floor(pole.mesh.position.y * 2) === 0 && !isCrawling)
+  return poles.some(
+    (pole) => Math.floor(pole.mesh.position.y * SANTA_COLLISION_THRESHOLD) === SANTA_Y_POSITION && !isCrawling
+  )
 }
 
 export function isPastFinishLine(finishLine: FinishLine): boolean {
-  return Math.floor(finishLine.mesh.position.y * 2) === 0
+  if (!finishLine) {
+    return false
+  }
+  return Math.floor(finishLine.mesh.position.y * SANTA_COLLISION_THRESHOLD) === SANTA_Y_POSITION
 }
